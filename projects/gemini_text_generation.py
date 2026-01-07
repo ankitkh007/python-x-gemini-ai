c8c25b1bdea34ab11ee34a1c8bcb726c7ecf13a4
ankitkh007 <ankit123khanna456@gmail.com>
2026-01-07 23:05:27 +0530

    testing gemini text generation

--- projects/gemini_text_generation.py
+++ projects/gemini_text_generation.py
@@ -1,14 +1,29 @@
 from core.gemini_client import client
 from google.genai import types
+from PIL import Image
 
-prompt = input("Enter any promt: ")
+# prompt = input("Enter any promt: ")
 
-## Testing Systeminstructions
+## Testing Systeminstructions, temperature
+# response = client.models.generate_content(
+#     model="gemini-2.5-flash",
+#     contents=prompt,
+#     config=types.GenerateContentConfig(
+#         system_instruction="Response should be within 50 words. Your resposes should be like sigma male.",
+#         temperature=0.5,
+#     ),
+# )
+
+
+## Testing Multimodal response
+image = Image.open("projects\images\sigma_boys.jpeg")
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents=prompt,
+    contents=[image, "Describe this image."],
     config=types.GenerateContentConfig(
-        system_instruction="Response should be within 50 words. Your resposes should be like sigma male.",
+        system_instruction="Response should be within 50 words. "
+        "Respond like you're roasting the subjects."
+        "No mercy Pure Roasting!",
         temperature=0.5,
     ),
 )
