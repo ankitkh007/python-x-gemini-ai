a94ad6c1f663bf6e1e1fa8b98caa5111c2c1ceb4
Ankit Khanna <115912370+ankitkh007@users.noreply.github.com>
2026-01-09 22:29:11 +0530

    Deleted projects/test.py

--- projects/test.py
+++
@@ -1,8 +0,0 @@
-from core.gemini_client import client
-
-prompt = input("Enter any promt: ")
-response = client.models.generate_content(model="gemini-2.5-flash", contents=prompt)
-
-print("Gemini response: ")
-print("----------------------------------------")
-print(response.text)
