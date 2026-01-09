00a64755053179510f8ccb06acfceb49561a2010
ankitkh007 <ankit123khanna456@gmail.com>
2026-01-09 22:32:22 +0530

    AI-Travel-Agent

---
+++ projects/AI-Travel-Agent/ai_travel_agent.py
@@ -0,0 +1,7 @@
+from core.gemini_client import client
+
+
+def main():
+    print("Welcome to Yatri-AI!! Let's plan your journey.")
+    source = input("Enter your Source: ")
+    destination = input("Enter your Destination: ")
